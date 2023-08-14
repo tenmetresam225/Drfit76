53501c7eac27a2b8e1c996ae16ab3fc15a827ef5
Sam <0321714@schoolsnet.act.edu.au>
2023-08-14 13:13:38 +1000

    Main updated with track drawing utils.py added for image scaling

--- Main.py
+++ Main.py
@@ -1,21 +1,36 @@
 import pygame
 import time
 import math
+from utils import scale_image
 
-GRASS = pygame.image.load("Images/grass.jpg")
-TRACK = pygame.image.load("Images/track.png")
+GRASS = scale_image(pygame.image.load("Images/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("Images/track.png"), 0.9)
 
-TRACK_BORDER = pygame.image.load("Images/track-border.png")
+TRACK_BORDER = scale_image(pygame.image.load("Images/track-border.png"), 0.9)
 FINISH = pygame.image.load("Images/finish.png")
-RED_CAR = pygame.image.load("Images/red-car.png")
-GREEN_CAR = pygame.image.load("Images/green-car.png")
+RED_CAR = scale_image(pygame.image.load("Images/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("Images/green-car.png"), 0.55)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Racing Game!")
+pygame.display.set_caption("Drift76")
+
+FPS = 60
+
+def draw(win, images):
+    for img, pos in images:
+        win.blit(img, pos)
 
 run = True
+clock = pygame.time.Clock()
+images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+
 while run:
+    clock.tick(FPS)
+
+    draw(WIN, images)
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
