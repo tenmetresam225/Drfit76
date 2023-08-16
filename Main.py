be93f4a58be5a9a7fbf09ca7bd0dc2440b4a397c
Sam <0321714@schoolsnet.act.edu.au>
2023-08-16 12:58:40 +1000

    Car is now drawn and has rotation functions

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import pygame
 import time
 import math
-from utils import scale_image
+
+from utils import scale_image, blit_rotate_center
 
 GRASS = scale_image(pygame.image.load("Images/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("Images/track.png"), 0.9)
@@ -10,6 +11,7 @@ TRACK_BORDER = scale_image(pygame.image.load("Images/track-border.png"), 0.9)
 FINISH = pygame.image.load("Images/finish.png")
 RED_CAR = scale_image(pygame.image.load("Images/red-car.png"), 0.55)
 GREEN_CAR = scale_image(pygame.image.load("Images/green-car.png"), 0.55)
+MAIN_CAR = scale_image(pygame.image.load("Images/main-car.png"), 0.55)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,22 +19,47 @@ pygame.display.set_caption("Drift76")
 
 FPS = 60
 
-def draw(win, images):
+class AbstractCar:
+    def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x, self.y = self.START_POS
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+
+class PlayerCar(AbstractCar):
+    IMG = MAIN_CAR
+    START_POS = (180, 200)
+def draw(win, images, player_car):
     for img, pos in images:
         win.blit(img, pos)
+
+    player_car.draw(win)
+    pygame.display.update()
 
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+player_car = PlayerCar(4, 4)
 
 while run:
     clock.tick(FPS)
 
-    draw(WIN, images)
-    pygame.display.update()
+    draw(WIN, images, player_car)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
+
 pygame.quit()
