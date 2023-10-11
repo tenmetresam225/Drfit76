c7d81506393d856ba0f97495538502b830af7cc6
Ruben <dylanjp@iinet.net.au>
2023-10-11 12:14:31 +1100

    Changing speed variables

--- Main.py
+++ Main.py
@@ -46,12 +46,12 @@ FPS = 60
 class AbstractCar:
     def __init__(self, max_vel, rotation_vel):
         self.img = self.IMG
-        self.max_vel = 3
+        self.max_vel = 2
         self.vel = 0
         self.rotation_vel = rotation_vel
         self.angle = 0
         self.x, self.y = self.START_POS
-        self.acceleration = 0.5
+        self.acceleration = 0.1
 
     def rotate(self, left=False, right=False):
         if left:
