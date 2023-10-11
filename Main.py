fbde0cff0de406443ed6b0f4c16d4f1c9c7eca4c
Ruben <dylanjp@iinet.net.au>
2023-10-11 12:06:29 +1100

    Changing speed variables

--- Main.py
+++ Main.py
@@ -51,7 +51,7 @@ class AbstractCar:
         self.rotation_vel = rotation_vel
         self.angle = 0
         self.x, self.y = self.START_POS
-        self.acceleration = 0.05
+        self.acceleration = 0.01
 
     def rotate(self, left=False, right=False):
         if left:
