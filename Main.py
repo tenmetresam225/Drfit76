d74463eb91c7305833a263de9ff48b6593a13868
Sam <0321714@schoolsnet.act.edu.au>
2023-10-13 10:21:11 +1100

    CPU added
    (NOTE FOR SAM)
    FIX THE PATHING, THE CAR AIMS FOR TOP LEFT CORNER

--- Main.py
+++ Main.py
@@ -23,7 +23,6 @@ if uname[1] == "raspberrypi":
     button_blue_left = Button(10)
     button_blue_right = Button(9)
 
-
 from utils import scale_image, blit_rotate_center
 
 GRASS = scale_image(pygame.image.load("Images/grass.jpg"), 2.5)
@@ -43,6 +42,8 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("Drift76")
 
 FPS = 60
+PATH = [(190, 87), (235, 50), (487, 50), (506, 107), (486, 180), (401, 185), (316, 171), (262, 209), (291, 249), (353, 253), (415, 255), (478, 255), (504, 282), (503, 321), (503, 361), (505, 452), (489, 490), (448, 500), (408, 485), (396, 444), (404, 393), (404, 347), (380, 320), (348, 311), (320, 311), (294, 324), (279, 347), (270, 381), (267, 415), (273, 443), (278, 464), (260, 488), (228, 497), (201, 487), (166, 450), (126, 409), (93, 371), (48, 331), (34, 277), (33, 193), (29, 124), (32, 68), (69, 38), (102, 39), (126, 61), (130, 104), (128, 153), (114, 194), (104, 228), (105, 258), (135, 285), (167, 286), (193, 254), (189, 220), (186, 165), (186, 126)]
+
 
 class AbstractCar:
     def __init__(self, max_vel, rotation_vel):
@@ -70,7 +71,7 @@ class AbstractCar:
 
     def move_backward(self):
         moving = True
-        self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
+        self.vel = max(self.vel - self.acceleration, -self.max_vel / 2)
         self.move()
 
     def move(self):
@@ -99,6 +100,7 @@ class AbstractCar:
         poi = mask.overlap(car_mask, offset)
         return poi
 
+
 class ComputerCar(AbstractCar):
     IMG = RIVAL_CAR
     START_POS = (175, 200)
@@ -109,17 +111,57 @@ class ComputerCar(AbstractCar):
         self.current_point = 0
         self.vel = max_vel
 
-    def draw_points(self,win):
-            for point in self.path:
-                pygame.draw.circle(win(255, 0, 0), point, 5)
+    def draw_points(self, win):
+        for point in self.path:
+            pygame.draw.circle(win, (255, 0, 0), point, 5)
 
     def draw(self, win):
         super().draw(win)
+
         self.draw_points(win)
+
+    def calculate_angle(self):
+        target_x, target_y = self.path[self.current_point]
+        x_diff = target_x - self.x
+        y_diff = target_y - self.y
+
+        if y_diff == 0:
+            desired_radian_angle = math.pi / 2
+        else:
+            desired_radian_angle = math.atan(x_diff/y_diff)
+
+        if target_y > self.y:
+            desired_radian_angle += math.pi
+
+        difference_in_angle = self.angle - math.degrees(desired_radian_angle)
+        if difference_in_angle >= 180:
+            difference_in_angle -= 360
+
+        if difference_in_angle > 0:
+            self.angle -= min(self.rotation_vel, abs(difference_in_angle))
+        else:
+            self.angle += min(self.rotation_vel, abs(difference_in_angle))
+
+    def update_path_point(self):
+        target = self.path[self.current_point]
+        rect = pygame.Rect(self.x, self.y, self.img.get_width(), self.img.get_height())
+        if rect.collidepoint(*target):
+            self.current_point += 1
+
+
+    def move(self):
+        if self.current_point >= len(self.path):
+            return
+
+        self.calculate_angle()
+        self.update_path_point()
+        super().move()
 
 class PlayerCar(AbstractCar):
     IMG = MAIN_CAR
     START_POS = (190, 200)
+
+
 def draw(win, images, player_car, computer_car):
     for img, pos in images:
         win.blit(img, pos)
@@ -128,28 +170,28 @@ def draw(win, images, player_car, computer_car):
     computer_car.draw(win)
     pygame.display.update()
 
-def Main(x,y):
+
+def Main(x, y):
     WIN.blit(MENU, (0, 0))
     pygame.display.update()
+
 
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
 player_car = PlayerCar(2, 4)
-computer_car = ComputerCar(2, 4)
+computer_car = ComputerCar(2, 4, PATH)
 Menu = False
 
 while run:
     clock.tick(FPS)
 
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            computer_car.path.append(pos)
+
+    computer_car.move()
 
     keys = pygame.key.get_pressed()
     moved = False
@@ -176,10 +218,10 @@ while run:
             pygame.time.wait(1500)
             print("FREESIA")
         if button_bottom_left.is_pressed:
-                moved = True
-                moving = True
-                forward = True
-                player_car.move_forward()
+            moved = True
+            moving = True
+            forward = True
+            player_car.move_forward()
         if button_bottom_middle.is_pressed:
             moved = True
             moving = True
@@ -210,5 +252,6 @@ while run:
     if player_car.collide(TRACK_BORDER_MASK) != None:
         player_car.bounce()
 
-print(computer_car.path)
+    print(computer_car.path)
+print("diddly doo")
 pygame.quit()
